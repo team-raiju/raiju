f76580d3880a669430af55767657b6f33f592985
Daniel Nery <danielnso97@gmail.com>
2019-12-02 00:06:55 -0300

    Tech Challenge RC

--- src/main.c
+++ src/main.c
@@ -10,6 +10,18 @@
 #include "mcu.h"
 #include "fsm.h"
 
+#include "motors.h"
+#include "buttons.h"
+#include "radio.h"
+#include "mcu.h"
+#include "buzzer.h"
+#include "leds.h"
+#include "distance.h"
+#include "line.h"
+#include "debug.h"
+#include "utils.h"
+#include "strategies.h"
+
 /*****************************************
  * Private Constant Definitions
  *****************************************/
@@ -18,12 +30,35 @@
  * Main Function
  *****************************************/
 
-fsm_t main_fsm;
+// fsm_t main_fsm;
 
 int main(void) {
-    fsm_init(&main_fsm);
+    // fsm_init(&main_fsm);
+
+    // for (;;) {
+    // fsm_cycle(&main_fsm);
+    // }
+
+    //// RC
+    mcu_init();
+
+    // debug_init();
+    motors_init();
+    radio_init();
+    buzzer_init();
+
+    motors_drive(0, 0);
+    mcu_sleep(1000);
+
+    for (int i = 0; i < 5; i++) {
+        led4_toggle();
+        mcu_sleep(50);
+    }
 
     for (;;) {
-        fsm_cycle(&main_fsm);
+        coordinate_t coords = radio_get_coordinates();
+
+        motors_drive(coords.x, coords.y);
+        mcu_sleep(5);
     }
 }
